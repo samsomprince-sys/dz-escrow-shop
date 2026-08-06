3809bc672e0f83547b500f6fe4adca7ddd0a9a59
SamirBoulefaa <samisaw40@gmail.com>
2026-08-06 16:38:48 +0100

    fix absolute inputs layout

--- app/merchant/add-product/page.tsx
+++ app/merchant/add-product/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from '../../../lib/supabase';
+import { supabase } from '../../lib/supabase';
+
 
 export default function AddProductPage() {
   const [title, setTitle] = useState('');
