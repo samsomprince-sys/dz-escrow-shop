f9a8d38edfc431e1abd07dd11d7d4c29354e59ee
SamirBoulefaa <samisaw40@gmail.com>
2026-08-07 15:31:06 +0100

    fix local image visibility on vercel by setting images unoptimized to true in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* هنا تضع إعدادات مشروعك السابقة وتجاوز الفحص مؤقتاً */
+  images: {
+    unoptimized: true, // هذا السطر يفرض على فيرسيل قراءة وعرض أي صورة داخل مجلد public فوراً دون حجبها
+  },
 };
 
 export default nextConfig;
